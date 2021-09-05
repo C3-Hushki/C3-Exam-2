1ce6430eba2199359a73639602775ab7805cd4db
hushki94 <omarhushki94@gmail.com>
2021-09-05 20:52:53 +0400

    Q1 and Q2

--- part_1/part_1.js
+++ part_1/part_1.js
@@ -25,3 +25,57 @@ Q9) When the `submit` button is clicked (from Q6) it should add a new book in th
 Q10) When rendering the book list (from Q7) add a delete button next to each book, that when clicked will delete the corresponding book from the database and the state.
 
 */
+
+
+
+
+
+// Q1) Connect the server to a database. 
+const mongoose = require('mongoose')
+const options = {
+    useUrlParser:true,
+    useCreateIndex:true,
+    useUifiedIndex:true
+}
+mongoose.connect('mongodb://localhost:27017/Omar_Hushki', options ,(res)=>{
+    console.log('....Connected')
+},(err)=>{
+    console.log(err)
+} )
+
+
+
+
+//Q2) Create an API `/books` to insert a new book to the database, it should have (title , author, pages, publisher, published_at)
+
+//bookSchema
+const mongoose = require('mongoose')
+
+const bookSchema = mongoose.Schema({
+    title : {type:String} , 
+    author :{type:String} ,
+    pages:{type:Number}, 
+    publisher:{type:String}, 
+    published_at:{type:Date}
+})
+
+const bookModel = mongoose.model('Book' , bookSchema)
+
+//controller
+const createBook = (req,res)=>{
+    const {title , author, pages, publisher, published_at} = req.body
+
+    const newBook = new bookModel({
+        title , 
+        author, 
+        pages, 
+        publisher, 
+        published_at
+    })
+
+    newBook.save().then((result)=>{
+        res.status(200).json({success:true , message:'Book Created'})
+    }).catch((err)=>{
+        res.status(200).json({success:false , message:'server error'})
+    })
+}
